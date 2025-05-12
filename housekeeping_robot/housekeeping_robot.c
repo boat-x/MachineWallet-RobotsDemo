b40d63fd82c5be7fd81dfea79dd7f492688aa945
gary <flyingfft@gmail.com>
2025-05-12 23:25:41 +0800

    Robots Demo of BoAT-based Machine Wallet

--- housekeeping_robot/housekeeping_robot.c
+++ housekeeping_robot/housekeeping_robot.c
@@ -76,7 +76,7 @@ static fibo_mdc_ProfileRef_t g_profileRef = NULL;
 const BCHAR * g_rpcUrl = "https://base-mainnet.infura.io";
 const BUINT32 g_chain_id = 8453;
 
-const BCHAR * g_erc20_contract_address_str = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // USDC on Base mainnet
+const BCHAR * g_erc20_contract_address_str = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"; // USDC on Base mainnet
 
 BoatEthWallet *g_ethereum_wallet_ptr;
 BUINT8 g_keypairIndex = 0;
